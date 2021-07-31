0bb99765f0a161da244da3cef4c5de7bf21fe72c
Manasés Jesús <contact@manasesjesus.com>
2021-07-31 18:24:00 +0200

    adding exercise 01

---
+++ ex01/hello.ts
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+const input1 = document.querySelector("num1")! as HTMLInputElement;
+const input2 = document.querySelector("num2")! as HTMLInputElement;
+
+function add (num1: number, num2: number) {
+    return num1 + num2;
+}
+
+console.log(add(+input1.value, +input2.value));
