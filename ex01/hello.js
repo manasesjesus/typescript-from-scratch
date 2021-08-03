1d9cc8b745feff4e33700b7f3ba3e1b417c3cc13
Manasés Jesús <contact@manasesjesus.com>
2021-08-03 20:39:29 +0200

    adding tsc init config file

--- ex01/hello.js
+++
@@ -1,7 +0,0 @@
-var button = document.querySelector("button");
-var input1 = document.querySelector("num1");
-var input2 = document.querySelector("num2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-console.log(add(+input1.value, +input2.value));
